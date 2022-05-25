89e2b97c2c71841484291cbf8bc1d65cffbc5de7
Afelix997 <105471577+Afelix997@users.noreply.github.com>
2022-05-24 22:28:49 -0700

    Update 99_bottles.js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,13 +1,14 @@
 function bottleSong(numBeer) {
-  
- while (numBeer>1){
-    console.log(` ${numBeer} bottles of beer on the wall, ${numBeer} bottles of beer.`);
-     if (numBeer>2){ 
-         console.log(` Take one down and pass it around, ${numBeer-1} bottles of beer on the wall.`)
-     } else { console.log(` Take one down and pass it around, 1 bottle of beer on the wall.`)}
-        numBeer--
-   } console.log(` 1 bottle of beer on the wall, 1 bottle of beer.\n Take one down and pass it around, 
-   no more bottles of beer on the wall.\n No more bottles of beer on the wall, no more bottles of beer.\n 
-   Go to the store and buy some more, ${numBeer} bottles of beer on the wall.`)
-}
-
+  let tot=numBeer
+  while (numBeer>1){
+     console.log(`${numBeer} bottles of beer on the wall, ${numBeer} bottles of beer.`);
+      if (numBeer>2){ 
+          console.log(`Take one down and pass it around, ${numBeer-1} bottles of beer on the wall.`)
+      } else { console.log(`Take one down and pass it around, 1 bottle of beer on the wall.`)}
+         numBeer--
+    } console.log(`1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, 
+  no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer.\n 
+  Go to the store and buy some more, ${tot} bottles of beer on the wall.`)
+ }
+ 
+ bottleSong(10)
